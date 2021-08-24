7a171e5ef29c47a38fc8169b57a2c580de758b14
Lucas Michailian <lucasmichailian@gmail.com>
2021-08-24 20:55:51 -0300

    add reset and register page

--- App.tsx
+++ App.tsx
@@ -2,6 +2,8 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { SafeAreaView, StyleSheet } from "react-native";
 import LoginPage from "./src/pages/LoginPage";
+import RegisterPage from "./src/pages/RegisterPage";
+import ResetPage from "./src/pages/ResetPage";
 import { dimen } from "./src/utils";
 
 export default function App(test: string) {
---
+++ src/pages/RegisterPage.tsx
@@ -0,0 +1,75 @@
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+import Button from "../components/common/Button";
+import RoundedButton from "../components/common/RoundedButton";
+import TextInput from "../components/common/TextInput";
+import TextLink from "../components/common/TextLink";
+import { colors, dimen } from "../utils";
+
+function RegisterPage() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.mainContent}>
+        <Text style={styles.title}>Register</Text>
+        <TextInput placeholder="Fullname" />
+        <TextInput keyboardType={"email-address"} placeholder="Email" />
+        <TextInput secureTextEntry placeholder="Password" />
+        <TextInput secureTextEntry placeholder="Confirm" />
+        <Button onPress={() => console.log("test")}>Register</Button>
+        <TextLink
+          onPress={() => alert("forget")}
+          align="center"
+          style={styles.join}
+        >
+          Are you a member ? Log In
+        </TextLink>
+      </View>
+      <Text style={styles.bottomText}>Or register with social</Text>
+      <View style={styles.socialButtons}>
+        <RoundedButton
+          size={28}
+          icon={"facebook"}
+          style={styles.facebookButton}
+        />
+        <RoundedButton size={28} icon={"google"} />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  mainContent: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: dimen.fontSize.extraBig,
+    fontWeight: dimen.fontWeight.extraBold,
+    marginBottom: dimen.baseUnit * 2,
+  },
+  bottomText: {
+    color: colors.secondary,
+    textAlign: "center",
+  },
+
+  link: {
+    marginBottom: dimen.baseUnit * 2,
+  },
+  join: {
+    marginVertical: dimen.baseUnit * 2,
+  },
+  socialButtons: {
+    justifyContent: "center",
+    flexDirection: "row",
+    paddingTop: dimen.baseUnit * 2,
+  },
+  facebookButton: {
+    marginRight: dimen.baseUnit * 2,
+  },
+});
+
+export default RegisterPage;
